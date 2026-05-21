a6130549d6da6cf268f5cb2fefb171cda8c080ec
yappe414-glitch <yappe414@gmail.com>
2026-05-22 08:54:30 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'calc-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  'https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
